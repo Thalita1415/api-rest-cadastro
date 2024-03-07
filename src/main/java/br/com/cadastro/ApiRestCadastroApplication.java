f5d541d4cc4bf8d7af06df39853e241fd3a42942
Thalita <Thali@DESKTOP-QJHH801>
2024-03-07 19:47:20 -0300

    Criação do projeto

---
+++ src/main/java/br/com/cadastro/ApiRestCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apirestcadastro/ApiRestCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apirestcadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
